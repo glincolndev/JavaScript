150495cefe55c39aad0c2b549b821d46c1092325
Guilherme Lincoln <dev.glincoln@gmail.com>
2024-03-27 22:12:02 -0300

    Comentário do código
    Adição de comentários para facilitar a navegação pelo código e melhor documentação.

--- Roda A Roda/rdr.js
+++ Roda A Roda/rdr.js
@@ -1,3 +1,20 @@
+/* 
+Olá! Aqui começa o nosso código.
+
+Importante: Todos os outputs são via console, mas exibição 
+via HTML será implementada.
+
+Entretanto, qualquer usuário com pouca familiaridade às devTools
+deve ser capaz de jogar com pouca dificuldade, dado que
+tanto o palpite quanto a verificação são botões intuitivos.
+
+Futuramente, instruções serão mostradas no HTML ao abrir a página.
+*/
+
+/*
+Primeiro, a função sortear() irá usar a biblioteca Math para
+retirar um índice aleatório do banco de palavras (inicializado como "gabarito")
+*/
 function sortear() {
   var gabarito = [
     ["G", "A", "T", "O"],
@@ -17,6 +34,8 @@ function sortear() {
   return resultSort;
 }
 
+// Vetor onde serão armazenados os inputs
+
 var tentativa = [];
 
 function input() {
@@ -29,17 +48,36 @@ function input() {
 }
 
 var letra = input();
-
 var resultSort = sortear();
+/*
+Este vetor servirá para implentação futura de um
+evento de erro, para maior engajamento do usuário 
+e também para que seja facilitada a tomada de decisão
+*/
+var erros = [];
 
+/*
+Criando este loop,
+acertos[] agora tem o mesmo número de elementos
+da palavra gabarito e vai atuar como a nossa versão
+censurada dela, que irá se revelando conforme
+tentativas bem sucedidas
+*/
 var acertos = [];
 for (a in resultSort) {
   acertos.push("_");
 }
-console.log(acertos);
 
-var erros = [];
 
+
+/* 
+checar() é o coração do jogo.
+Esta função passa por todos as letras que estão
+em tentativas[] e por todo o vetor da palavra misteriosa,
+verificando a cada letra nova que o usuário envia
+se é correspondente ou não, para daí atualizar o vetor acertos[]
+ou erros[].
+*/
 function checar() {
   for (b in tentativa) {
     for (a in resultSort) {
@@ -52,5 +90,3 @@ function checar() {
   console.log(`Acertos: ${acertos}`);
   //console.log(`Erros: ${erros}`);
 }
-
-
